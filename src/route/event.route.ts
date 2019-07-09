ed64bb577c0bdb7ad0ced79ab6a362c465490920
zwisler <alex-z-@web.de>
2019-07-09 14:15:29 +0200

    feat: remove all events from website

--- src/route/event.route.ts
+++ src/route/event.route.ts
@@ -5,30 +5,35 @@ import { EventService } from '../service/event.service';
 
 @Route({ basePath: '/event' })
 export class EventRoute {
-  constructor(private eventService: EventService) {}
+    constructor(private eventService: EventService) {}
 
-  @Endpoint({ method: 'POST' })
-  async create(websiteId: string, name: string, @CustomParam('cookies') cookies) {
-    await this.eventService.create(websiteId, name, cookies.sa);
-  }
+    @Endpoint({ method: 'POST' })
+    async create(websiteId: string, name: string, @CustomParam('cookies') cookies) {
+        await this.eventService.create(websiteId, name, cookies.sa);
+    }
 
-  @Endpoint({ method: 'GET' })
-  async get(websiteId: string) {
-    return await this.eventService.getByWebsite(websiteId);
-  }
+    @Endpoint({ method: 'GET' })
+    async get(websiteId: string) {
+        return await this.eventService.getByWebsite(websiteId);
+    }
 
-  @Endpoint()
-  async events(websiteId: string, eventName: string) {
-    return this.eventService.getEvents(websiteId, eventName);
-  }
+    @Endpoint()
+    async clear(websiteId: string) {
+        return this.eventService.clearEvents(websiteId);
+    }
 
-  @Endpoint()
-  async uniqueVisitors(websiteId: string) {
-    return this.eventService.getUniqueVisiors(websiteId);
-  }
+    @Endpoint()
+    async events(websiteId: string, eventName: string) {
+        return this.eventService.getEvents(websiteId, eventName);
+    }
 
-  @Endpoint({ method: 'GET' })
-  async eventCountByVisitor(websiteId: string) {
-    return await this.eventService.eventCountByVisitor(websiteId);
-  }
+    @Endpoint()
+    async uniqueVisitors(websiteId: string) {
+        return this.eventService.getUniqueVisiors(websiteId);
+    }
+
+    @Endpoint({ method: 'GET' })
+    async eventCountByVisitor(websiteId: string) {
+        return await this.eventService.eventCountByVisitor(websiteId);
+    }
 }
--- src/service/event.service.ts
+++ src/service/event.service.ts
@@ -8,40 +8,48 @@ import { BridgeError } from '@zwisler/bridge/core/util/bridge.error';
 
 @Service()
 export class EventService {
-  eventRepo: Repository<TrackingEvent>;
+    eventRepo: Repository<TrackingEvent>;
 
-  constructor(private ormService: OrmService, private websiteService: WebsiteService) {
-    this.eventRepo = this.ormService.connection.getRepository(TrackingEvent);
-  }
-  async create(websiteId: string, name: string, owner: string, data = '') {
-    const website = await this.websiteService.getById(websiteId);
-    if (!website) throw new BridgeError(404, 'Website not found!');
-    const event = new TrackingEvent(name, owner, data, website);
-    this.eventRepo.save(event);
-  }
+    constructor(private ormService: OrmService, private websiteService: WebsiteService) {
+        this.eventRepo = this.ormService.connection.getRepository(TrackingEvent);
+    }
+    async create(websiteId: string, name: string, owner: string, data = '') {
+        const website = await this.websiteService.getById(websiteId);
+        if (!website) throw new BridgeError(404, 'Website not found!');
+        const event = new TrackingEvent(name, owner, data, website);
+        this.eventRepo.save(event);
+    }
 
-  async getByWebsite(websiteId: string) {
-    return await this.eventRepo.find({ where: { website: websiteId } });
-  }
+    async getByWebsite(websiteId: string) {
+        return await this.eventRepo.find({ where: { website: websiteId } });
+    }
 
-  getEvents(websiteId: string, event: string): Promise<TrackingEvent[]> {
-    return this.eventRepo.find({ where: { website: websiteId, name: event } });
-  }
+    clearEvents(websiteId: string) {
+        return this.eventRepo
+            .createQueryBuilder()
+            .delete()
+            .where('website = :websiteId', { websiteId })
+            .execute();
+    }
 
-  async getUniqueVisiors(websiteId: string) {
-    return (await this.eventRepo
-      .createQueryBuilder()
-      .select('DISTINCT owner')
-      .where({ website: websiteId })
-      .getRawMany()).length;
-  }
+    getEvents(websiteId: string, event: string): Promise<TrackingEvent[]> {
+        return this.eventRepo.find({ where: { website: websiteId, name: event } });
+    }
 
-  async eventCountByVisitor(websiteId: string) {
-    const a = this.eventRepo
-      .createQueryBuilder()
-      .select('COUNT(*) as EventCount, TrackingEvent.owner as Visitor')
-      .where({ website: websiteId })
-      .groupBy('TrackingEvent.owner');
-    return await a.getRawMany();
-  }
+    async getUniqueVisiors(websiteId: string) {
+        return (await this.eventRepo
+            .createQueryBuilder()
+            .select('DISTINCT owner')
+            .where({ website: websiteId })
+            .getRawMany()).length;
+    }
+
+    async eventCountByVisitor(websiteId: string) {
+        const a = this.eventRepo
+            .createQueryBuilder()
+            .select('COUNT(*) as EventCount, TrackingEvent.owner as Visitor')
+            .where({ website: websiteId })
+            .groupBy('TrackingEvent.owner');
+        return await a.getRawMany();
+    }
 }
