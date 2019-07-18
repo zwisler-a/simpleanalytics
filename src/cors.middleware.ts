ab493442fd782059d9657290aeb941318f9b3237
zwisler <alex-z-@web.de>
2019-07-18 23:13:28 +0200

    fix: use prod env

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -2,7 +2,7 @@ export function allowCrossDomain (req, res, next) {
     res.header('Access-Control-Allow-Origin', req.get('origin'));
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
     if ('OPTIONS' === req.method) {
         res.send(200);
     }
--- src/sa/env/prod.js
+++ src/sa/env/prod.js
@@ -1,1 +1,2 @@
 export const REDIRECT_URL = 'http://auth.zwisler.dev/';
+export const BASE_URL = '';
